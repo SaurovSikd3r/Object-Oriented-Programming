7073ec1a0fd7bc6c5aaf0a098af3fb723af09b2f
Saurov Sikder <109725864+SaurovSikd3r@users.noreply.github.com>
2025-03-19 22:19:46 +0600

    Add files via upload

---
+++ Java/ArithmeticOperations.java
@@ -0,0 +1,11 @@
+public class ArithmeticOperations {
+    public static void main(String[] args) {
+
+        double grade1 = 3.5;
+        double grade2 = 5.4;
+        double grade3 = 9.9;
+
+        double average = (grade1 + grade2 + grade3)/3;
+        System.out.println("The average is:" + average);
+    }
+}

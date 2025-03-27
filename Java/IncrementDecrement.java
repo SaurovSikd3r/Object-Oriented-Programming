bb7f192489ad5326eba64ecde00cf5d5226f1a10
Saurov Sikder <109725864+SaurovSikd3r@users.noreply.github.com>
2025-03-27 19:46:26 +0600

    Add files via upload

---
+++ Java/IncrementDecrement.java
@@ -0,0 +1,8 @@
+public class IncrementDecrement {
+    public static void main(String[] args) {
+        int i = 5;
+        System.out.println("Old i = " + i);
+        i++;
+        System.out.println("New i " + i);
+    }
+}
